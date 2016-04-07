8bb2696141eb77cedab64f803d45e5d0adde93ba
aytsai <ariana.tsai@gmail.com>
2016-04-06 18:31:53 -0700

    'minor'

--- static/js/ryss.js
+++ static/js/ryss.js
@@ -110,6 +110,22 @@ function init_map2() {
 }
 
 /* TODO: animated scroll */
-$('a[href^="#"]').on('click', function() { 
-  window.scrollTo(window.scrollX, window.scrollY - 300);
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      if ($(window).scrollTop() <= 5) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 700);
+      }
+      else {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 50
+        }, 700);
+      }
+      return false;
+    }
+  }
 });
